e1b2fe4c19189d25d50f09aa5b177afb252ef44d
Noel Cruz <noel.j.cruz@outlook.com>
2020-02-13 22:48:40 -0800

    Created Navbar component and added a basic div.

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <h1>Hey Noel</h1>
+            <h2>From Kristina</h2>
+        </div>
+    )
+}
+
+
+export default Navbar;
